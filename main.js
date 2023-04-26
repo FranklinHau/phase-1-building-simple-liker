759cd109b70f399467afa0a3613227d15423702a
Franklin Hau <fhauchi@gmail.com>
2023-04-26 15:39:22 -0600

    simple-liker

--- main.js
+++ main.js
@@ -3,7 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.getElementById('modal');
+const modalMessage = document.getElementById('modal-message');
+const heartButtons = document.querySelectorAll('.like-glyph');
 
+modal.classList.add('hidden');
+
+function handleHeartClick(event) {
+  const heart = event.target;
+
+  mimicServerCall()
+    .then(() => {
+      
+      if (heart.textContent === EMPTY_HEART) {
+        heart.textContent = FULL_HEART;
+        heart.classList.add('activated-heart');
+      } else {
+        heart.textContent = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
+      }
+    })
+    .catch((error) => {
+     
+      modal.classList.remove('hidden');
+      modalMessage.textContent = error;
+      setTimeout(() => {
+        modal.classList.add('hidden');
+      }, 3000);
+    });
+}
+
+
+heartButtons.forEach(heart => heart.addEventListener('click', handleHeartClick));
 
 
 
